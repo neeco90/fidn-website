1d005da7a81ac9e365d97ead3e89858af8d8e498
Nicolas Castano <nicolasc@lagash.com>
2018-05-29 14:44:49 -0300

    nav fixed on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
+/* function checkSize(){
+    //Si es vista mobile
+    if ($(".ph-show").css("display") == "block" ){
+    }
+
+    //Si es vista tablet
+    if ($(".mb-show").css("display") == "block" && $(".ph-show").css("display") == "none"){
+    }
+} */
+
 $(document).ready(function(){
     var docencia = $("#docencia");
     var submenu = $("#submenu");
@@ -16,4 +26,18 @@ $(document).ready(function(){
     submenu.mouseleave(function(){
         submenu.removeClass("open");
     });
+
+    /* // run test on initial page load
+    checkSize();
+    
+    // run test on resize of the window
+    $(window).resize(checkSize);
+
+    //Si es vista desktop
+    if ($(".ph-show").css("display") == "none" && $(".mb-show").css("display") == "none"){
+    } */
+
+    var sticky = new Waypoint.Sticky({
+        element: $('.navbar .row')[0]
+    })
 })
