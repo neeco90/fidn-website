4a7ddd4064fb0921a637b9bdc7bf69c05fdf3bcc
Nicolás Castano <nicolas.castano@quantumfactory.com.ar>
2018-06-24 02:20:17 -0300

    seccion contacto

--- public/js/main.js
+++ public/js/main.js
@@ -36,6 +36,65 @@ function showMore(){
     $('#showMore').hide();
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function notEmpty(input){
+    return $(input).val() != '';
+}
+
+function validateForm(event){
+    var inputs = $('.input[required]').toArray();
+
+    inputs.forEach(input => {
+
+        //Chequeo por cada campo y muestro o no clase error
+        if($(input).val() != ''){
+
+            //Chequeo si el email tiene formato correcto
+            if($(input).attr("id") == "email"){
+                var email = $("#email").val();
+
+                if (validateEmail(email)) {
+                    $(input).removeClass('error');
+                } else {
+                    $(input).addClass('error');
+                }
+            } else{
+                $(input).removeClass('error');
+            }
+            
+        } else{
+            $(input).addClass('error');
+        }
+    });
+    
+    //Chequeo si hay algun campo con error    
+    var checkNotEmpty = inputs.every(notEmpty);
+    var notErrors = $(".input.error");
+
+    //Si no hay ningun error
+    if(notErrors.length == 0){
+        //-----------------ENVIAR FORMULARIO-----------------//
+        $('#errorMsg').hide();
+        $('#formatErrorMsg').hide();
+        $(".input").val("").removeClass("error");
+        
+        openModal();
+
+    } else if(!checkNotEmpty){ //Si hay algun campo vacio
+        $('#errorMsg').show();
+        $('#formatErrorMsg').hide();
+    } else{ //Si hay error en formato
+        $('#errorMsg').hide();
+        $('#formatErrorMsg').show();
+    }
+
+    event.stopPropagation();
+}
+
 $(document).ready(function(){
     var docencia = $(".navbar__menu #docencia");
     var submenu = $(".navbar__menu #submenu");
